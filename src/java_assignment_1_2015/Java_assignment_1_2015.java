0d787163e7a0be819ec88cb4f3bf23c235e94b04
c0664341 <c0664341@SB-N208-22.lambton.on.ca>
2015-09-16 00:59:31 -0400

    Assignment1-2015

---
+++ src/java_assignment_1_2015/Java_assignment_1_2015.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_assignment_1_2015;
+
+/**
+ *
+ * @author Parth Patel
+ */
+public class Java_assignment_1_2015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         /************************************************
+         * Assignment #1
+         * job posting: java developer
+         * reqirements:
+         * J2EE : appicaton software. https://en.wikipedia.org/wiki/Java_-
+         * Platform,_Enterprise_Edition
+         * JQery : Javascript library . https://en.wikipedia.org/wiki/JQuery
+         * Html : to create web pages . https://en.wikipedia.org/wiki/HTML
+         * Css:change the style of web page. https://en.wikipedia.org/wiki/-
+         * Cascading_Style_Sheets
+         * oracle database : storage.https://en.wikipedia.org/wiki/
+         * Oracle_Database
+         * toronto salary - 78k
+         * vancouver salary-82k
+         * waterloo-80k
+         * 
+         */
+    }
+    
+}
